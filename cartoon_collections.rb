625f8fc73349a07b52bbb0ddde045c412103a4da
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-07 23:20:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(planeteers)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
+  
 end
 
 def find_the_cheese# code an argument here

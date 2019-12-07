feeadc26797019ff1d0f79e9704eb5600214732f
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-07 23:43:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteers)
 end
 
 def long_planeteer_calls(calls)
-    if calls.select { |len| len.length > 4}
+    if calls.any? { |len| len.length > 4}
       return true
     else 
       return false 

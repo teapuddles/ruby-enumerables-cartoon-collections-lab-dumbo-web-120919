a086cb98384e6e5b014ead6058a87956bc8f16a0
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-07 23:44:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |call, index|
     puts "#{index + 1}. #{call}\n"
@@ -18,7 +20,8 @@ def long_planeteer_calls(calls)
     end
   end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(aos)
+  binding.pry
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
